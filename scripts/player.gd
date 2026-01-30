f6dd96c9b95bd2aad4631e4a00d4bc0dbbc27d55
turtletennis <stephenjenkins42@gmail.com>
2026-01-30 21:36:38 +0000

    Add phantom camera, basic movement, CSG based level

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,1 +1,12 @@
 extends CharacterBody3D
+@export var speed:float = 10
+var input_dir:Vector2
+
+func _process(delta: float) -> void:
+    input_dir = Input.get_vector("left","right","up","down")
+
+func _physics_process(delta: float) -> void:
+    velocity.x = input_dir.x * speed
+    velocity.y = input_dir.y * speed * -1
+    velocity.z = 0
+    move_and_slide()
