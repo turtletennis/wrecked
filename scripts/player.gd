584ac8d90454cda4623adc7e28a3db7192e48948
D4nDude <hobbs.daniel.007@gmail.com>
2026-01-31 18:00:53 +0000

    feat: Update level and clamp to sealevel

--- scripts/player.gd
+++ scripts/player.gd
@@ -14,34 +14,36 @@ var input_dir:Vector2
 @export var blindness_rate:float = 0.1
 @export var blindness_drain_rate:float = 5.0
 func _ready() -> void:
-    animationPlayer.play(swimAnimationName)
-    animationPlayer.pause()
+	animationPlayer.play(swimAnimationName)
+	animationPlayer.pause()
 func _process(delta: float) -> void:
-    input_dir = Input.get_vector("left","right","up","down")
+	input_dir = Input.get_vector("left","right","up","down")
 
 func _physics_process(delta: float) -> void:
-    
-    velocity.x = input_dir.x * speed
-    velocity.y = input_dir.y * speed * -1
-    velocity.z = 0
-    if(input_dir.length_squared() > deadZone):
-        var desiredAngle =3*PI/2 - input_dir.angle()
-        var angle = lerp_angle(global_rotation.z,desiredAngle,rotationSpeed*delta)
-        rotation =Vector3(rotation.x,rotation.y,angle)
-        if(!animationPlayer.is_playing()):
-            animationPlayer.play(swimAnimationName)
-    else:
-        animationPlayer.pause()
-    move_and_slide()
+	
+	velocity.x = input_dir.x * speed
+	velocity.y = input_dir.y * speed * -1
+	velocity.z = 0
+	if(input_dir.length_squared() > deadZone):
+		var desiredAngle =3*PI/2 - input_dir.angle()
+		var angle = lerp_angle(global_rotation.z,desiredAngle,rotationSpeed*delta)
+		rotation =Vector3(rotation.x,rotation.y,angle)
+		if(!animationPlayer.is_playing()):
+			animationPlayer.play(swimAnimationName)
+	else:
+		animationPlayer.pause()
+	move_and_slide()
 
-    # mask blindness
-    if !GameController.player_in_air and position.y < 0:
-        blindness += blindness_rate
-    
-    if Input.is_physical_key_pressed(KEY_E):
-        if GameController.player_in_air or position.y > 0:
-            blindness -= blindness_drain_rate
-        else:
-            blindness += blindness_drain_rate
+	# mask blindness
+	if !GameController.player_in_air and position.y < 0:
+		blindness += blindness_rate
+	
+	if Input.is_physical_key_pressed(KEY_E):
+		if GameController.player_in_air or position.y > 0:
+			blindness -= blindness_drain_rate
+		else:
+			blindness += blindness_drain_rate
 
-    blindness = clampf(blindness, 0.0, 100.0)
+	blindness = clampf(blindness, 0.0, 100.0)
+
+	position.y = clampf(position.y, -1000.0, 1.0)
