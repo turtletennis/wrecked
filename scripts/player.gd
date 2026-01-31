1b808097e4cf2dc38b7e3cfd4a04d1445a1ac7c1
turtletennis <stephenjenkins42@gmail.com>
2026-01-31 11:10:03 +0000

    Smoothly rotate player in the direction they're "swimming"

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,13 +3,19 @@ extends CharacterBody3D
 class_name Player
 
 @export var speed:float = 10
+@export var rotationSpeed:float = 25
+var deadZone:float = 0.1
 var input_dir:Vector2
 
 func _process(delta: float) -> void:
-    input_dir = Input.get_vector("left","right","up","down")
+	input_dir = Input.get_vector("left","right","up","down")
 
 func _physics_process(delta: float) -> void:
-    velocity.x = input_dir.x * speed
-    velocity.y = input_dir.y * speed * -1
-    velocity.z = 0
-    move_and_slide()
+	velocity.x = input_dir.x * speed
+	velocity.y = input_dir.y * speed * -1
+	velocity.z = 0
+	if(input_dir.length_squared() > deadZone):
+		var desiredAngle =PI/2 - input_dir.angle()
+		var angle = lerp_angle(global_rotation.z,desiredAngle,rotationSpeed*delta)
+		rotation =Vector3(rotation.x,rotation.y,angle)
+	move_and_slide()
